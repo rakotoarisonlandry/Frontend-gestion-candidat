aeef001fd165aa31ba042eeb8edafad2c4856b1e
rakotoarison landry <rakotorisonlandry@gmail.com>
2026-04-08 09:15:54 +0200

    chore: remove obsolete test results and clean up app.spec.ts

--- tests/e2e/app.spec.ts
+++ tests/e2e/app.spec.ts
@@ -5,18 +5,3 @@ test("page loads", async ({ page }) => {
 
   await expect(page.locator("text=Login")).toBeVisible();
 });
-test("page loads", async ({ page }) => {
-  await page.goto("/candidates");
-
-  await expect(page.locator("text=Candidates")).toBeVisible();
-});
-test("page loads", async ({ page }) => {
-  await page.goto("/candidates");
-
-  await expect(page.locator("text=Ajouter Candidat")).toBeVisible();
-});
-test("page loads", async ({ page }) => {
-  await page.goto("/");
-
-  await expect(page.locator("text=invalid credentials")).toBeVisible();
-});
